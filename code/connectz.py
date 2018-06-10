48ef5acdb4e7b42e41b4a40c98e53a975d8b85ea
jason <jason@burnapps.co.uk>
2018-06-10 11:00:14 +0100

    Command line check.

---
+++ code/connectz.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def ConnectZ(object):
+    def __init__(self, file_input=None):
+        # Will always need a file
+        pass
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        # Incorrect command line argument count. Alert user
+        sys.stdout.write('connectz.py: Provide one \033[92minput \033[0mfile')  # Assumes ANSI available (Linux default)
+
