bc13edf24e6aaf2b7f2db0513f05a0dde93e0a04
hbcweb <jason@hbc.co.uk>
2018-06-11 17:03:36 +0100

    Testing completed against challenge file definitions.

--- code/connectz.py
+++ code/connectz.py
@@ -5,7 +5,7 @@
 #
 # The idea is to hold the game in a matrix that can not exceed the board height/width restrictions defined in the
 # game file.
-# Game outcome is decided by tests against the `active` board area to save processing. For instance if game coins are
+# Game outcome is decided by tests against the `active` board area to save processing. For instance, if game coins are
 # only within columns 1 to 3, there is no need to test for a game win outside of a virtual board width of '3'.
 # Similarly for coin stack height.
 #
@@ -41,37 +41,56 @@ class GameException(Error):
         self.code = code
 
 
-# Holds the
+# Holds and handles anything to do with the ConnectZ game.
 class Game(object):
-    _board_width = 0  # Maximum board width
-    _board_height = 0  # Maximum board height
-    _counters = 0  # Connect `n`
-    # Define active area
-    _active_game_start_column = 0
-    _active_game_end_column = 0
-    # _active_game_start_row = 0
-    _active_game_end_row = 0
-    # Game matrix
-    _the_game = []
-    # Lets keep track of the number of moves made
-    _move_count = 0
-    # Game conditions
-    _player_one_win = False
-    _player_two_win = False
-    _draw = False
-    _incomplete = True
+    # _board_width = 0  # Maximum board width
+    # _board_height = 0  # Maximum board height
+    # _counters = 0  # Connect `n`
+    # # Define active area
+    # _active_game_start_column = 0
+    # _active_game_end_column = 0
+    # # _active_game_start_row = 0
+    # _active_game_end_row = 0
+    # # Game matrix
+    # _the_game = []
+    # # Lets keep track of the number of moves made
+    # _move_count = 0
+    # # Game conditions
+    # _player_one_win = False
+    # _player_two_win = False
+    # _draw = False
+    # _incomplete = True
 
     def __init__(self, conf):
         """
         Game instantiation takes configuration settings.
         :param conf: Game configuration
         """
+        # Define instance variables.
+        self._board_width = 0  # Maximum board width
+        self._board_height = 0  # Maximum board height
+        self._counters = 0  # Connect `n`
+        # Define active area
+        self._active_game_start_column = 0
+        self._active_game_end_column = 0
+        # _active_game_start_row = 0
+        self._active_game_end_row = 0
+        # Game matrix
+        self._the_game = []
+        # Lets keep track of the number of moves made
+        self._move_count = 0
+        # Game conditions
+        self._player_one_win = False
+        self._player_two_win = False
+        self._draw = False
+        self._incomplete = True
+
         # Split config line and attempt to cast them as ints
         try:
             lst_configs = [int(conf) for conf in conf.rstrip('\n').split()]
         except Exception:
             raise GameException(8)
-        # Check config count
+        # Check config value count
         if len(lst_configs) != 3:
             raise GameException(8)  # Code 8 -- Invalid file
         # And that the game conditions are valid
@@ -88,45 +107,177 @@ class Game(object):
         :return: int
         """
         if self._player_one_win:
-            return 0
-        if self._player_two_win:
             return 1
-        if self._draw:
+        if self._player_two_win:
             return 2
+        if self._draw:
+            return 0
         if self._incomplete:
             return 3
 
-    def _check_horizontal_win(self):
+    def _check_counters(self, lst_counters):
+        """
+        Check if the argument list of counters are all the same.
+        :type lst_counters: list(int)
+        :rtype: bool
+        """
+        if lst_counters.count(1) == self._counters:
+            # Player 1 wins!
+            self._draw = self._player_two_win = self._incomplete = False
+            self._player_one_win = True
+            return True
+        if lst_counters.count(2) == self._counters:
+            # Player 2 wins!
+            self._draw = self._player_one_win = self._incomplete = False
+            self._player_two_win = True
+            return True
         return False
+
+    def _check_horizontal_win(self):
+        """
+        Check to see if a player has `n` in a row.
+        :rtype: bool
+        """
+        b_have_winner = False
+        # Build counter lists and check if all counters are the same
+        # On a win update outcome and return True
+        for row_idx in range(self._active_game_end_row):
+            # Moving up row
+            for column_idx in range(self._active_game_start_column - 1, self._active_game_end_column):
+                # Moving along columns
+                these_counters = []
+                b_check_counters = True
+                for count in range(self._counters):
+                    # Extracting coins
+                    try:
+                        these_counters.append(self._the_game[row_idx][column_idx + count])
+                    except IndexError:
+                        b_check_counters = False
+                        break  # Cell doesn't exist therefore end of row reached
+                if b_check_counters:
+                    # Test these counters
+                    if self._check_counters(these_counters):
+                        # We have a winner
+                        b_have_winner = True
+                        return b_have_winner
+            # Then move to next row and repeat
+        return b_have_winner
 
     def _check_vertical_win(self):
-        return False
+        """
+        Check to see if a player has `n` in a column.
+        :rtype: bool
+        """
+        b_have_winner = False
+        # Build counter lists and check if all counters are the same
+        # On a win update outcome and return True
+        for row_idx in range(self._active_game_end_row):
+            # Moving up row
+            for column_idx in range(self._active_game_start_column - 1, self._active_game_end_column):
+                # Moving along columns
+                these_counters = []
+                b_check_counters = True
+                for count in range(self._counters):
+                    # Extracting coins
+                    try:
+                        these_counters.append(self._the_game[row_idx + count][column_idx])
+                    except IndexError:
+                        b_check_counters = False
+                        break  # Cell doesn't exist therefore top of board reached
+                if b_check_counters:
+                    # Test these counters
+                    if self._check_counters(these_counters):
+                        # We have a winner
+                        b_have_winner = True
+                        return b_have_winner
+            # Then move to next row and repeat
+        return b_have_winner
 
     def _check_diagonal_incline_win(self):
-        return False
+        """
+        Check to see if a player has `n` in a inclining diagonal.
+        :rtype: bool
+        """
+        b_have_winner = False
+        # Build counter lists and check if all counters are the same
+        # On a win update outcome and return True
+        for row_idx in range(self._active_game_end_row - 1):
+            # Moving up row
+            for column_idx in range(self._active_game_start_column - 1, self._active_game_end_column - 1):
+                # Moving along columns
+                these_counters = []
+                b_check_counters = True
+                for count in range(self._counters):
+                    # Extracting coins
+                    try:
+                        these_counters.append(self._the_game[row_idx + count][column_idx + count])
+                    except IndexError:
+                        b_check_counters = False
+                        break  # Cell doesn't exist therefore edge of board reached
+                if b_check_counters:
+                    # Test these counters
+                    if self._check_counters(these_counters):
+                        # We have a winner
+                        b_have_winner = True
+                        return b_have_winner
+            # Then move to next row and repeat
+        return b_have_winner
 
     def _check_diagonal_decline_win(self):
-        return False
+        """
+        Check to see if a player has `n` in a declining diagonal.
+        :rtype: bool
+        """
+        b_have_winner = False
+        # Build counter lists and check if all counters are the same
+        # On a win update outcome and return True
+        for row_idx in range(self._active_game_end_row - 1, -1, -1):
+            # Moving down rows from top
+            for column_idx in range(self._active_game_start_column - 1, self._active_game_end_column - 1):
+                # Moving along columns
+                these_counters = []
+                b_check_counters = True
+                for count in range(self._counters):
+                    # Extracting coins
+                    try:
+                        if row_idx - count < 0:
+                            raise IndexError  # Gone off bottom of board!
+                        these_counters.append(self._the_game[row_idx - count][column_idx + count])
+                    except IndexError:
+                        b_check_counters = False
+                        break  # Cell doesn't exist therefore edge of board reached
+                if b_check_counters:
+                    # Test these counters
+                    if self._check_counters(these_counters):
+                        # We have a winner
+                        b_have_winner = True
+                        return b_have_winner
+            # Then move to next row and repeat
+        return b_have_winner
 
-    def _check_for_win(self):
+    def _process_board(self):
         """
-        This is the main game engine and determines if the game has a winner or it is a draw.
-        Sets one of the game booleans on a completion state.
+        This is the main game engine and determines if the game has a winner or if the game is a draw.
+        Sets one of the game outcome booleans on a completion state.
         """
-        print(self._the_game)
-        # No point running check unless minimum moves exceeded
-        if self._move_count/2 > self._counters:
+        # Check to see if the game is already complete.
+        if self._player_one_win or self._player_two_win or self._draw:
+            # One too many moves, raise an exception
+            raise GameException(4)  # code 4 -- Illegal continue
+        # No point running check unless minimum moves reached
+        if self._move_count - (self._counters - 1) < self._counters:
             return False
         # Only interested in active board space
-        if ((self._active_game_end_column - self._active_game_start_column) + 1 < self._counters) and (self._active_game_end_row + 1 < self._counters):
+        if ((self._active_game_end_column - self._active_game_start_column) + 1 < self._counters) and (
+                self._active_game_end_row + 1 < self._counters):
             return False  # The active board area can't accommodate a win
-        if self._active_game_end_row + 1 < self._counters:
+        if self._active_game_end_row < self._counters:
             # Only horizontal wins will work
             return self._check_horizontal_win()
         if (self._active_game_end_column - self._active_game_start_column) + 1 < self._counters:
             # Only vertical wins can work due to horizontal distribution of counters
-            return self._check_vertical_win
-        # Check them in turn
+            return self._check_vertical_win()
+        # We have an active board area that requires all win vectors to be tested
         if self._check_horizontal_win():
             return True
         if self._check_vertical_win():
@@ -135,13 +286,18 @@ class Game(object):
             return True
         if self._check_diagonal_decline_win():
             return True
+        # Check for draw condition
+        if self._move_count == (self._board_width * self._board_height):
+            # All spaces populated with counters, therefore a draw
+            self._player_one_win = self._player_two_win = self._incomplete = False
+            self._draw = True
+            return True
         return False
-
 
     def move(self, column, player):
         """
         What player and which row is used to update the the game board.
-        When the board has been updated the `completion` function is run to determine if the game has finished.
+        When the board has been updated the `win checker` function is run to determine if the game has finished.
         :param column: str
         :param player: int 1 or 2
         :return:
@@ -168,8 +324,8 @@ class Game(object):
             new_row = [0] * self._board_width
             new_row[column - 1] = 1  # And add counter for player 1
             self._the_game.append(new_row)  # Update the board
-            self._active_game_start_column = self._active_game_end_column = column - 1  # And update active game space
-            self._active_game_end_row = 0
+            self._active_game_start_column = self._active_game_end_column = column  # And update active game space
+            self._active_game_end_row = 1
         else:
             b_coin_placed = False
             row_idx = 0
@@ -191,29 +347,44 @@ class Game(object):
                 if len(self._the_game) > self._board_height:
                     raise GameException(5)  # code 5 -- Illegal row
             # Update active board space
-            if column - 1 < self._active_game_start_column:
-                self._active_game_start_column = column - 1
-            if self._active_game_end_column < column - 1:
-                self._active_game_end_column = column - 1
-            if self._active_game_end_row < row_idx:
-                self._active_game_end_row = row_idx
+            if column < self._active_game_start_column:
+                self._active_game_start_column = column
+            if self._active_game_end_column < column:
+                self._active_game_end_column = column
+            if self._active_game_end_row < row_idx + 1:
+                self._active_game_end_row = row_idx + 1
         self._move_count += 1  # Keep an eye in the number of moves
-        # Now the board is updated, lets check for and end game event
-        return self._check_for_win()
+        # Now the board is updated, lets process the moves made
+        return self._process_board()
 
 
 class ConnectZ(object):
-    _outcome = None
 
     def __init__(self, file_input=None):
-        # Test if file exists
-        this_file = Path(file_input)
-        if not this_file.is_file():
+        """
+        Class instantiation requires file to be processed.
+        :param file_input: ASCII file on disk
+        """
+        self._this_file = None  # Game file to be processed
+        try:
+            # Test if file exists
+            self._this_file = Path(file_input)
+        except Exception as e:
+            raise GameException(9)
+        if not self._this_file.is_file():
             # There is no file on disk therefore raise exception
             raise GameException(9)  # code 9 -- File error
+
+    def run_game(self):
+        """
+        Run through the file provided at instantiation. The file should contain the game configuration on the the first
+        row followed by player moves on separate lines.
+        :return: int
+        :exception: GameException
+        """
         obj_game = None
         players_go = 1  # Player 1 goes first
-        with open(file_input) as f:
+        with open(self._this_file) as f:
             for line in f:
                 if obj_game is None:
                     # First row contains game config
@@ -226,34 +397,22 @@ class ConnectZ(object):
                         players_go = 2
                     else:
                         players_go = 1
-
-    def get_outcome(self):
-        return self._outcome
+        # All moves completed with no exceptions made. Return valid game state
+        return obj_game.get_outcome()
 
 
 # Command line execution
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         # Incorrect command line argument count. Alert user
-        sys.stdout.write(
-            '\rconnectz.py: Provide one \033[92minput \033[0mfile')  # Assumes ANSI available (Linux default)
-        print()  # Tidy command prompt
+        print('\rconnectz.py: Provide one \033[92minput \033[0mfile')  # Assumes ANSI available (Linux default)
     else:
         try:
             # Run the game. Exceptions raised for game errors otherwise valid outcome found in get_outcome()
-            sys.stdout.write(ConnectZ(sys.argv[1]).get_outcome())
-            print()
+            print(ConnectZ(sys.argv[1]).run_game())
         except GameException as e:
             # Game error codes reported to user.
-            sys.stdout.write(e.code)
-            print()
+            print(e.code)
         except Exception as e:
             # Not good if got to here.
             raise e
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-# ConnectZ("{path}/tests/classic.txt".format(path=dir_path))
-# ConnectZ("/Users/jason/Documents/My\ Development/connectz/code/tests/classic.txt")
-# ConnectZ("/usr/src/app/tests/classic.txt")
-# ConnectZ("/usr/src/app/tests/impossible.txt")
-ConnectZ("/usr/src/app/tests/too_many_rows.txt")
--- code/tests/test_game.py
+++ code/tests/test_game.py
@@ -1,58 +1,134 @@
+import os
+import sys
 import unittest
-# from ../connectz import Game
-# from ../.G import GameException
-from connectz import GameException
+
+sys.path.append(os.path.dirname(__file__) + '..')
 from connectz import Game
 from connectz import ConnectZ
 
 
 class TestGame(unittest.TestCase):
 
-    def test_game_valid_init(self):
-        """
-        Game accepts three values
-        """
-        self.assertTrue(Game(7, 6, 4))
-
     def test_game_invalid_init(self):
         """
-        Game accepts tree int values, two given should raise exception
+        Game accepts three string values, two given should raise exception
         """
         try:
-            Game(7, 6)
+            Game('7 6')
         except Exception as e:
-            self.assertEquals(e.code, 8)
+            self.assertEqual(e.code, 8)
 
     def test_game_invalid_init_type(self):
         """
-        Game accepts tree int values, strings given
+        Game accepts three string values that should cast to int.
         """
         try:
-            Game('a', 7, 6)
+            Game('a 7 6')
         except Exception as e:
-            self.assertEquals(e.code, 8)
+            self.assertEqual(e.code, 8)
 
     def test_incorrect_column_event(self):
         """
         Column outside of board dimensions
         """
         try:
-            obj_game = Game(2, 2, 2)
+            obj_game = Game('2 2 2')
             obj_game.move(3, 1)
         except Exception as e:
-            self.assertEquals(e.code, 6)
+            self.assertEqual(e.code, 6)
 
     def test_for_missing_file(self):
         try:
             ConnectZ("/does/not/exist/file.txt")
         except Exception as e:
-            self.assertEquals(e.code, 9)
+            self.assertEqual(e.code, 9)
 
     def test_classic_connect(self):
         """
         Run classic connect four game where players alternate columns
         """
-        self.assertEquals(ConnectZ("/usr/src/app/classic.txt"), 1)
+        self.assertEqual(ConnectZ("/usr/src/app/tests/classic.txt").run_game(), 1)
+
+    def test_draw(self):
+        """
+        Run draw game
+        """
+        self.assertEqual(ConnectZ("/usr/src/app/tests/draw.txt").run_game(), 0)
+
+    def test_illegal_column(self):
+        """
+        Run illegal column game
+        """
+        try:
+            ConnectZ("/usr/src/app/tests/illegal_column.txt").run_game()
+        except Exception as e:
+            self.assertEqual(e.code, 6)
+
+    def test_illegal_continue(self):
+        """
+        Run illegal continue game
+        """
+        try:
+            ConnectZ("/usr/src/app/tests/illegal_continue.txt").run_game()
+        except Exception as e:
+            self.assertEqual(e.code, 4)
+
+    def test_illegal_game(self):
+        """
+        Run illegal game
+        """
+        try:
+            ConnectZ("/usr/src/app/tests/illegal_game.txt").run_game()
+        except Exception as e:
+            self.assertEqual(e.code, 7)
+
+    def test_illegal_row(self):
+        """
+        Run illegal row
+        """
+        try:
+            ConnectZ("/usr/src/app/tests/illegal_row.txt").run_game()
+        except Exception as e:
+            self.assertEqual(e.code, 5)
+
+    def test_incomplete(self):
+        """
+        Run incomplete game
+        """
+        self.assertEqual(ConnectZ("/usr/src/app/tests/incomplete.txt").run_game(), 3)
+
+    def test_invalid_file(self):
+        """
+        Test an invalid file
+        """
+        try:
+            ConnectZ("/usr/src/app/tests/invalid_file.txt").run_game()
+        except Exception as e:
+            self.assertEqual(e.code, 8)
+
+    def test_player_one_win(self):
+        """
+        Run player 1 wins scenario
+        """
+        self.assertEqual(ConnectZ("/usr/src/app/tests/player_one_win.txt").run_game(), 1)
+
+    def test_player_two_win(self):
+        """
+        Run player 2 wins scenario
+        """
+        self.assertEqual(ConnectZ("/usr/src/app/tests/player_two_win.txt").run_game(), 2)
+
+    def test_player_one_win_diagonal_up(self):
+        """
+        Run player 1 wins with diagonal scenario
+        """
+        self.assertEqual(ConnectZ("/usr/src/app/tests/player_one_win_incline.txt").run_game(), 1)
+
+    def test_player_one_win_diagonal_down(self):
+        """
+        Run player 1 wins with diagonal scenario
+        """
+        self.assertEqual(ConnectZ("/usr/src/app/tests/player_one_win_decline.txt").run_game(), 1)
 
 
 if __name__ == '__main__':
